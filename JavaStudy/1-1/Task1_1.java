19d4570d46e647b86ea78c89f55a1df22daad0ae
cringyjianice <68896181+cringyjianice@users.noreply.github.com>
2020-07-30 11:48:53 +0900

    commit
    commit to local respository

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,23 @@
+public class Task1_1 {
+    public static void main(String[] args) {
+        // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+        // int hogeInt = 50;
+        // ④変数 hogeInt を値「100」で上書き
+        int hogeInt = 100;
+        
+        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+        // String hogeString = "テスト";
+        // ⑤変数 hogeString を値「合格」で上書き
+        String hogeString = "合格";
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // [double型を宣言して、「hogeDouble」という変数名をつけて、0.2で初期化する。]
+        double hogeDouble = 0.2; 
+    
+        System.out.println(hogeInt);
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // [このコードの指示は、変数hogeStringの値を表示します。]
+        System.out.println(hogeString);
+        System.out.println(hogeDouble);
+
+    }
+}
---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,25 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+        
+        String[] country = {"JAPAN", "AMERICA", "KOREA"};
+
+        System.out.println(country.length);
+        
+        String[] strArray = new String[3];
+        strArray[0] = "りんご";
+        strArray[1] = "もも";
+        strArray[2] = "ぶどう";
+        
+        System.out.println(strArray[1]);
+        
+        //⑤下記の処理について、何をしているのかコメントを記入してください。
+        //[10、20、30、40、50 を要素の値とする、
+        //intArrayという名前のint型配列を宣言する。]
+        int[] intArray = { 10, 20, 30, 40, 50 };
+
+        // ⑥下記の処理について、何をしているのかコメントを記入してください。
+        // [intArrayという配列のindexが１と4の要素の値を表示する。]
+        System.out.println(intArray[1] + intArray[4]);
+
+    }
+}
